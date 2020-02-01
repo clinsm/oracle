24e52950fe2eacc9b5b650c58de2ba30436dd8b2
clinsm <30928377+clinsm@users.noreply.github.com>
2020-02-01 20:15:21 +0200

    Add files via upload

--- js/bit.js
+++ js/bit.js
@@ -21,3 +21,30 @@ hom[i].style.color = "white";
 }
 }
 }
+//for the slide thing on our work
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
